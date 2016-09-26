ae753893ca15d3d14c9473c4c1c242a4d55b191e
Paul McLaughlin <designer226@gmail.com>
2016-09-26 11:30:29 -0500

    Add files via upload

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,1 +1,10 @@
+$(document).on('click','.navbar-collapse.in',function(e) {
+	if( $(e.target).is('a') ) {
+		$(this).collapse('hide');
+	}
+});
 
+$(".nav a").on("click", function(){
+  $(".nav").find(".active").removeClass("active");
+  $(this).parent().addClass("active");
+});
